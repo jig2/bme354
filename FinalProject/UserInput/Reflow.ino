9383482ce5cc9ceef691977286d792815aeffc6a
jig2 <jig2@duke.edu>
2013-04-19 11:34:24 -0300

    Update Reflow.ino

--- FinalProject/UserInput/Reflow.ino
+++ FinalProject/UserInput/Reflow.ino
@@ -1,16 +1,13 @@
-float ReflowTemp()
+float selectReflowSlope()
 {
   float ReflowThresh;
 
-  int tens = 0;
   int ones = 0;
-
-  int place = 1;
 
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Select A Temp");
-  String ReflowString = String(tens) + String(ones);
+  lcd.print("Reflow Slope");
+  String ReflowString = String(ones);
   lcd.setCursor(0,1);
   lcd.print(ReflowString);
 
@@ -27,25 +24,15 @@ float ReflowTemp()
       }
     case btnUP:
       {
-        if (place==1) {
-          if (tens<9) {
-            tens = tens + 1;
-          } 
-          else {
-            tens = 0;
-          }
-        }
-        if (place==2) {
           if (ones<9) {
             ones = ones + 1;
           } 
           else {
             ones = 0;
           }
-        }
-        float ReflowThresh = tens*10 + ones;
+        float ReflowThresh = ones;
 
-        String ReflowString = String(tens) + String(ones);
+        String ReflowString = String(ones);
         lcd.setCursor(0,1);
         lcd.print(ReflowString);
         delay(400);
@@ -53,54 +40,14 @@ float ReflowTemp()
       }
     case btnDOWN:
       {
-        if (place==1) {
-          if (tens>0) {
-            tens = tens - 1;
-          } 
-          else {
-            tens = 9;
-          }
-        }
-        if (place==2) {
           if (ones>0) {
             ones = ones - 1;
           } 
           else {
             ones = 9;
           }
-        }
-        float ReflowThresh = tens*10 + ones;
-        String ReflowString = String(tens) + String(ones);
-        lcd.setCursor(0,1);
-        lcd.print(ReflowString);
-        delay(400);
-        break;
-      }
-    case btnLEFT:
-      {
-        if (place == 1) {
-          place = 2;
-        } 
-        else if (place == 2) {
-          place = 1;
-        } 
-        float ReflowThresh = tens*10 + ones;
-        String ReflowString = String(tens) + String(ones);
-        lcd.setCursor(0,1);
-        lcd.print(ReflowString);
-        delay(400);
-        break;
-      }
-    case btnRIGHT:
-      {
-        if (place == 1) {
-          place = 2;
-        } 
-        else {
-          place = 1;
-        }
-        float ReflowThresh = tens*10 + ones;
-        String ReflowString = String(tens) + String(ones);
+        float ReflowThresh = ones;
+        String ReflowString = String(ones);
         lcd.setCursor(0,1);
         lcd.print(ReflowString);
         delay(400);
@@ -108,21 +55,20 @@ float ReflowTemp()
       }
     case btnSELECT:
       {
-        float ReflowThresh = tens*10 + ones;
-        if (ReflowThresh>300 || ReflowThresh<20) {
+        float ReflowThresh = ones;
+        if (ReflowThresh>4 || ReflowThresh<1) {
           lcd.clear();
           lcd.setCursor(0,0);
-          lcd.print("You suck!");
+          lcd.print("Outside Range");
           lcd.setCursor(0,1);
           lcd.print("Resetting!");
           delay(5000);
-          tens = 0;
           ones = 0;
-          float ReflowThresh = tens*10 + ones;
-          String ReflowString = String(tens) + String(ones);
+          float ReflowThresh = ones;
+          String ReflowString = String(ones);
           lcd.clear();
           lcd.setCursor(0,0);
-          lcd.print("Select A Temp");
+          lcd.print("Select A Slope");
           lcd.setCursor(0,1);
           lcd.print(ReflowString);
           delay(400);
